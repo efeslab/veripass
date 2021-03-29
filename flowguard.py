33abfd033610e95ea9e006d0e3ad2c2281b0ef73
Jiacheng Ma <i@jcma.me>
2021-03-29 01:31:27 -0400

    add a pass to split arrays that are only accessed via a constant index

---
+++ flowguard.py
@@ -0,0 +1,114 @@
+import os
+import sys
+import pathlib
+import argparse
+import time
+from verilator import *
+from recordpass import *
+from passes.IdentifierRefPass import IdentifierRefPass
+from passes.TypeInfoPass import TypeInfoPass
+from passes.WidthPass import WidthPass
+from passes.CanonicalFormPass import CanonicalFormPass
+from passes.TaskSupportPass import TaskSupportPass
+from passes.common import PassManager
+#from livetest import *
+
+#sys.path.append(str(pathlib.Path(__file__).parent.absolute()/"build"/"lib.linux-x86_64-3.9"))
+start = time.time()
+
+from pyverilog.vparser.parser import VerilogCodeParser
+from pyverilog.dataflow.modulevisitor import ModuleVisitor
+from pyverilog.dataflow.signalvisitor import SignalVisitor
+from pyverilog.dataflow.bindvisitor import BindVisitor
+import pyverilog.utils.util as util
+
+from dataflowpass import dataflowtest
+
+from model.altsyncram_simple_model import AltsyncramSimpleModel
+from model.dcfifo_simple_model import DcfifoSimpleModel
+from model.scfifo_simple_model import ScfifoSimpleModel
+
+parser = argparse.ArgumentParser(description="Translate SystemVerilog to Readable Verilog")
+parser.add_argument("--top", dest="top_module", help="top module name")
+parser.add_argument("-F", dest="desc_file", help="description file path, similar to verilator")
+parser.add_argument("-o", dest="output", help="output path")
+parser.add_argument("--split", default=False, action="store_true", dest="split", help="whether to split variable")
+
+args = parser.parse_args()
+print("Top Module: {}".format(args.top_module))
+print("Desc File: {}".format(args.desc_file))
+print("Output Path: {}".format(args.output))
+print("Split Variables: {}".format(args.split))
+
+v = Verilator(top_module_name=args.top_module, desc_file=args.desc_file)
+ast = v.get_ast()
+
+#ri = RecordInstrument("xilinx", ast, v.get_splitted_typetable(), v.get_splitted_used_vars(), "pClk")
+#ri.add_data("ccip_std_afu__DOT__data_rx__BRA__511__03A480__KET__")
+#ri.add_data("ccip_std_afu__DOT__data_rx__BRA__479__03A448__KET__")
+#ri.add_data("ccip_std_afu__DOT__data_rx__BRA__447__03A416__KET__")
+#ri.add_trigger("ccip_std_afu__DOT__mpf__DOT__mpf_edge_fiu__DOT__b__DOT__c1_fifo__DOT__fifo__DOT__data__DOT__rbw__DOT__wen_q")
+#ri.add_trigger("ccip_std_afu__DOT__mpf__DOT__mpf_edge_fiu__DOT__wr_heap_data__DOT__c0__DOT__data__DOT__m_default__DOT__altsyncram_inst__DOT__i_rden_reg_a")
+#ri.generate()
+#ast = ri.ast
+
+used_vars = v.get_used_vars()
+typetable = v.get_typetable()
+
+module_visitor = ModuleVisitor()
+module_visitor.visit(ast)
+modulenames = module_visitor.get_modulenames()
+moduleinfotable = module_visitor.get_moduleinfotable()
+
+altsyncram = AltsyncramSimpleModel()
+dcfifo = DcfifoSimpleModel()
+scfifo = ScfifoSimpleModel()
+signal_visitor = SignalVisitor(moduleinfotable, "ccip_std_afu_wrapper")
+signal_visitor.addBlackboxModule("altsyncram", altsyncram)
+signal_visitor.addBlackboxModule("dcfifo", dcfifo)
+signal_visitor.addBlackboxModule("scfifo", scfifo)
+#signal_visitor.addBlackboxModule("scfifo")
+signal_visitor.start_visit()
+frametable = signal_visitor.getFrameTable()
+
+bind_visitor = BindVisitor(moduleinfotable, "ccip_std_afu_wrapper", frametable, noreorder=False, ignoreSyscall=True)
+bind_visitor.addBlackboxModule("altsyncram", altsyncram)
+bind_visitor.addBlackboxModule("dcfifo", dcfifo)
+bind_visitor.addBlackboxModule("scfifo", scfifo)
+#bind_visitor.addBlackboxModule("scfifo")
+bind_visitor.start_visit()
+dataflow = bind_visitor.getDataflows()
+terms = dataflow.getTerms()
+binddict = dataflow.getBinddict()
+
+dft = dataflowtest(ast, terms, binddict,
+        "ccip_std_afu_wrapper.c0Rx_data", "ccip_std_afu_wrapper.c0Rx_rspValid", "ccip_std_afu_wrapper.c1Tx_data",
+        "ccip_std_afu_wrapper.pck_cp2af_softReset",
+        gephi=True)
+dft.addBlackboxModule("altsyncram", altsyncram)
+dft.addBlackboxModule("dcfifo", dcfifo)
+dft.addBlackboxModule("scfifo", scfifo)
+dft.find2()
+
+termname = util.toTermname("ccip_std_afu_wrapper.c1Tx_data")
+t = terms[termname]
+b = binddict[termname]
+pm = PassManager()
+pm.register(IdentifierRefPass)
+pm.register(TypeInfoPass)
+pm.register(WidthPass)
+pm.register(CanonicalFormPass)
+pm.register(TaskSupportPass)
+pm.runAll(ast)
+
+codegen = ASTCodeGenerator()
+rslt = codegen.visit(ast)
+with open(args.output, 'w+') as f:
+    f.write(rslt)
+
+end = time.time()
+print(end - start)
+
+print("$display args:")
+for id, arg in enumerate(pm.state.display_args):
+    print("{}: {}".format(id, codegen.visit(arg)))
---
+++ passes/ArraySplitPass.py
@@ -0,0 +1,206 @@
+import pyverilog.vparser.ast as vast
+from passes.common import PassBase
+from passes.common import getDimensions
+from utils.ValueParsing import verilog_string_to_int
+
+import copy
+
+"""
+A full split (i.e. splitting an array to N independent variables) can be
+performed to an array that is only accessed with a constant index. We need
+to determine whether the array is eligible before doing the accutal split.
+"""
+
+class _ArrayPointerInfoPass(PassBase):
+    """
+    Will store the table of array's access info in pass_state
+
+    pass_state.array_access_info[array_name] == {True|False}
+    True means the array is eligible to a full split
+    """
+
+    def __init__(self, pass_state):
+        super().__init__(pass_state, True)
+        self.state.array_access_info = {}
+
+    def visit_Pointer(self, node):
+        if not node.var.name in self.state.array_access_info:
+            self.state.array_access_info[node.var.name] = True
+        if not isinstance(node.ptr, vast.IntConst):
+            self.state.array_access_info[node.var.name] = False
+
+
+"""
+This pass performs a full split to all eligible arrays.
+"""
+
+class _ArrayFullSplitPass(PassBase):
+    def __init__(self, pass_state):
+        super().__init__(pass_state, False)
+        self.array_access_info = self.state.array_access_info
+
+    def visit_ModuleDef(self, node):
+        new_items = []
+        for item in node.items:
+            if isinstance(item, vast.Variable):
+                if item.name in self.array_access_info:
+                    assert(item.dimensions != None)
+                    # access info is True, meaning it can be fully split
+                    if self.array_access_info[item.name]:
+                        new_name_tmpl = item.name + "__028" + "{}" + "__029"
+                        dims = getDimensions(item.dimensions)
+                        # Verilator shouldn't generate nested dimensions
+                        assert(len(dims) == 1)
+                        for i in range(0, dims[0]):
+                            new_item = copy.deepcopy(item)
+                            new_item.dimensions = None
+                            new_item.name = new_name_tmpl.format(i)
+                            new_items.append(new_item)
+                    else:
+                        new_items.append(item)
+                else:
+                    new_items.append(item)
+            else:
+                new_items.append(self.visit(item))
+        node.items = new_items
+        return node
+
+    def visit_Always(self, node):
+        node.statement = self.visit(node.statement)
+        return node
+
+    def visit_Constant(self, node):
+        return node
+
+    def visit_LConcat(self, node):
+        # Do not expect to see this syntax
+        assert(0 and "LConcat not implemented")
+
+    def visit_Concat(self, node):
+        new_list = []
+        for l in node.list:
+            new_list.append(self.visit(l))
+        node.list = new_list
+        return node
+
+    def visit_Cast(self, node):
+        node.value = self.visit(node.value)
+        return node
+
+    def visit_Repeat(self, node):
+        node.value = self.visit(node.value)
+        return node
+
+    def visit_Partselect(self, node):
+        node.var = self.visit(node.var)
+        return node
+
+    def visit_Pointer(self, node):
+        assert(isinstance(node.var, vast.Identifier))
+        assert(node.var.name in self.array_access_info)
+        # access info is True, meaning it can be fully split
+        if self.array_access_info[node.var.name]:
+            assert(isinstance(node.ptr, vast.IntConst))
+            new_name_tmpl = node.var.name + "__028" + "{}" + "__029"
+            idx = verilog_string_to_int(node.ptr.value)
+            new_name = new_name_tmpl.format(idx)
+            return vast.Identifier(new_name)
+        else:
+            return node
+
+    def visit_Lvalue(self, node):
+        node.var = self.visit(node.var)
+        return node
+
+    def visit_Rvalue(self, node):
+        node.var = self.visit(node.var)
+        return node
+
+    def visit_UnaryOperator(self, node):
+        node.right = self.visit(node.right)
+        return node
+
+    def visit_Operator(self, node):
+        node.left = self.visit(node.left)
+        node.right = self.visit(node.right)
+        return node
+
+    def visit_Cond(self, node):
+        node.cond = self.visit(node.cond)
+        node.true_value = self.visit(node.true_value)
+        node.false_value = self.visit(node.false_value)
+        return node
+
+    def visit_InstanceList(self, node):
+        new_instances = []
+        for i in node.instances:
+            new_instances.append(self.visit(i))
+        node.instances = new_instances
+        return node
+
+    def visit_Instance(self, node):
+        new_portlist = []
+        for p in node.portlist:
+            new_portlist.append(self.visit(p))
+        node.portlist = new_portlist
+        return node
+
+    def visit_PortArg(self, node):
+        node.argname = self.visit(node.argname)
+        return node
+
+    def visit_Identifier(self, node):
+        return node
+
+    def visit_Block(self, node):
+        assert(node.statements != None)
+        new_statements = []
+        for s in node.statements:
+            new_statements.append(self.visit(s))
+        node.statements = new_statements
+        return node
+
+    def visit_Assign(self, node):
+        node.left = self.visit(node.left)
+        node.right = self.visit(node.right)
+        return node
+
+    def visit_Initial(self, node):
+        node.statement = self.visit(node.statement)
+        return node
+
+    def visit_IfStatement(self, node):
+        node.cond = self.visit(node.cond)
+        if node.true_statement != None:
+            node.true_statement = self.visit(node.true_statement)
+        if node.false_statement != None:
+            node.false_statement = self.visit(node.false_statement)
+        return node
+    
+    def visit_Substitution(self, node):
+        node.left = self.visit(node.left)
+        node.right = self.visit(node.right)
+        return node
+
+    def visit_SingleStatement(self, node):
+        node.statement = self.visit(node.statement)
+        return node
+
+    def visit_SystemCall(self, node):
+        return node
+
+
+"""
+The interface which calls the info pass and the full split pass.
+"""
+
+class ArraySplitPass(PassBase):
+    def __init__(self, pass_state):
+        super().__init__(pass_state, False)
+        self.infopass = _ArrayPointerInfoPass(self.state)
+        self.fullsplitpass = _ArrayFullSplitPass(self.state)
+
+    def visit(self, node):
+        self.infopass.visit(node)
+        self.fullsplitpass.visit(node)
+
