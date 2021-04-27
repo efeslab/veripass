e890a268b1d89788f9a23b202c3e401aacd05e5f
Alkaid <zgf574564920@gmail.com>
2021-04-26 22:18:02 -0400

    Specify formatting/escaping rules as list of tuples.
    Also remove the "_" <-> "__05F" rule, which does not make sense to me.
    
    The original code incorrectly find "__05F" as the special character to
    escape. This rule also leads to undefined variable names.

--- utils/Format.py
+++ utils/Format.py
@@ -1,23 +1,26 @@
 import pyverilog.vparser.ast as vast
 
+# a list of tuples, (to_escape, escaped)
+escaping_rules = [
+    ("'", "__027"),
+    ("(", "__028"),
+    (")", "__029"),
+    (":", "__03A"),
+    (".", "__DOT__"),
+    ("[", "__BRA__"),
+    ("]", "__KET__"),
+]
+
+
 def beautify_name(name):
     s = name
-    s = s.replace("__028", "(")
-    s = s.replace("__029", ")")
-    s = s.replace("__03A", ":")
-    s = s.replace("__05F", "_")
-    s = s.replace("__DOT__", ".")
-    s = s.replace("__BRA__", "[")
-    s = s.replace("__KET__", "]")
+    for to_escape, escaped in escaping_rules:
+        s = s.replace(escaped, to_escape)
     return s
+
 
 def format_name(name):
     s = name
-    s = s.replace("(", "__028")
-    s = s.replace(")", "__029")
-    s = s.replace(":", "__03A")
-    s = s.replace("__05F", "_")
-    s = s.replace(".", "__DOT__")
-    s = s.replace("[", "__BRA__")
-    s = s.replace("]", "__KET__")
+    for to_escape, escaped in escaping_rules:
+        s = s.replace(to_escape, escaped)
     return s
